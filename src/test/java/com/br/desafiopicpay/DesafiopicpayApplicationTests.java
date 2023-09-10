61451cf5a81e03cfa489caca2b5e6f0745ac2ad9
Ueverson Ramos <ueverson.ramos@liax.tech>
2023-09-10 14:23:20 -0300

    first commit

---
+++ src/test/java/com/br/desafiopicpay/DesafiopicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiopicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
